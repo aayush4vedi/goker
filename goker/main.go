7da07acc93af4bcbc9a12b993d03d23437bbbf7a
4vediaayush <aayush.chaturvedi@razorpay.com>
2020-02-19 03:01:03 +0530

    Add ScoreEvaluator...phew

--- goker/main.go
+++ goker/main.go
@@ -6,14 +6,19 @@ import (
 	"github.com/goker"
 )
 
-const n, m int = 3, 2
-var p1, p2, table goker.Hand
+const n, m int = 5, 2
+
+var p1, p2 goker.Player
+var table goker.Hand
 
 func main() {
 	deck := goker.NewDeck()
 
 	//draw cards
-	p1, deck = goker.GetHand(n, deck)
-	p2, deck = goker.GetHand(n, deck)
+	p1.FiveCards, deck = goker.GetHand(n, deck)
+	p2.FiveCards, deck = goker.GetHand(n, deck)
 	table, deck = goker.GetHand(m, deck)
+
+	winner := goker.GetWinner(p1, p2, table)
+	fmt.Printf("Player %v won!\n", winner)
 }
